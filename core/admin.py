72c0b1a1d5d05cb2b1d364f55f3e7419865f2bbd
Sobraniee <141002334+Sobraniee@users.noreply.github.com>
2023-09-12 21:41:00 -0700

    my socialnet

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import *
 
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['name', 'likes', 'creator', 'status']
+    list_filter = ['status', 'creator']
+    search_fields = ['name', 'description', 'status', 'creator__username', 'creator__firstname']
+    list_editable = ['status']
+    inlines = [CommentInline]
+
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(Short)
--- socialnet/settings.py
+++ socialnet/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'socialnet.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "social_net_db",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        #"ENGINE": "django.db.backends.postgresql",
+        #"NAME": "social_net_copy",
         "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "127.0.0.1",
