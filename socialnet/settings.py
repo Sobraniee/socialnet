2e89118bf32dffe6f805e429bb26c555aca4cdd1
Sobraniee <141002334+Sobraniee@users.noreply.github.com>
2023-09-12 22:48:42 -0700

    sqlite settings

--- socialnet/settings.py
+++ socialnet/settings.py
@@ -82,14 +82,21 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        #"ENGINE": "django.db.backends.postgresql",
-        #"NAME": "social_net_copy",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         #"ENGINE": "django.db.backends.postgresql",
+#         #"NAME": "social_net_copy",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
